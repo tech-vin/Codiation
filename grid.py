a25181887b344b6c47c7a751e521296b2873e977
Vineet Singh <54138655+tech-vin@users.noreply.github.com>
2021-09-11 23:27:59 +0530

    Add files via upload

---
+++ grid.py
@@ -0,0 +1,137 @@
+import numpy as np
+import random
+class Grid(object):
+    def __init__(self):
+        self.current_grid = np.zeros((150,150))
+        self.section_tobe_evaluated = 0
+        self.size = 2
+        self.pattern = 'clear'
+        self.position = 0
+        self.position2 = 0
+        self.past_ticks = []
+
+    def new_pattern(self, pattern):
+        self.position2 = 0
+        current_grid = np.zeros((150, 150))
+        if pattern == 'glider':
+            glider = [[0, 0, 0, 0, 0],
+                      [0, 0, 1, 0, 0],
+                      [0, 0, 0, 1, 0],
+                      [0, 1, 1, 1, 0],
+                      [0, 0, 0, 0, 0]]
+            current_grid[11:16, 11:16] = glider
+            self.size = 2
+            self.current_grid = current_grid
+        elif pattern =='glider 2': # found on http://pentadecathlon.com/lifenews/
+            glider2 = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 1, 0, 0, 1, 0, 0, 1, 1, 0],
+                       [0, 0, 1, 1, 0, 1, 0, 0, 1, 0],
+                       [0, 0, 0, 1, 0, 1, 0, 1, 0, 0],
+                       [0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+                       [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+            self.size = 8
+            current_grid[10:18, 9:19] = glider2
+            self.current_grid = current_grid
+        elif pattern == 'glider gun':
+            glider_gun = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+                          [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+            current_grid[13: 24, 13: 51] = glider_gun
+            self.size = 36
+            self.current_grid = current_grid
+        elif pattern == 'random':
+            rand_sect1 = random.randint(0, 5)
+            rand_sect2 = random.randint(6, 100)
+            sect = current_grid[rand_sect1:rand_sect2, rand_sect1:rand_sect2]
+            for y in range(len(sect)):
+                for x in range(len(sect)):
+                    sect[y][x] = random.choice([1, 0])
+            self.current_grid = current_grid[rand_sect1:rand_sect2, rand_sect1:rand_sect2]
+        elif pattern == 'clear':
+            self.current_grid = current_grid
+        else:
+            return
+        self.pattern = pattern
+
+    def start_over(self):
+        self.position2 = 0
+        self.new_pattern(self.pattern)
+
+    def tick(self):
+        self.expand()
+        next_grid = np.zeros((self.size+ self.position,self.size+self.position))
+        current_grid = self.current_grid.tolist() # not using numpy functions because slows process down
+        for i in range(self.section_tobe_evaluated[2]-2, self.section_tobe_evaluated[3]+2):
+            for i2 in range(self.section_tobe_evaluated[0]-2,self.section_tobe_evaluated[1]+2):
+                neighbors = 0
+                try:
+                    current_state = current_grid[i][i2] # Can be either 1 or 0
+                except IndexError:
+                    current_state = 0
+                try:
+                    neighbors += current_grid[i + -1][i2 + -1]
+                    neighbors += current_grid[i + -1][i2 + 0]
+                    neighbors += current_grid[i + -1][i2 + 1]
+                    neighbors += current_grid[i + 0][i2 + -1]
+                    neighbors += current_grid[i + 0][i2 + 1]
+                    neighbors += current_grid[i + 1][i2 + -1]
+                    neighbors += current_grid[i + 1][i2 + 0]
+                    neighbors += current_grid[i + 1][i2 + 1]
+                except IndexError:
+                   continue
+
+                next_grid[i+self.position][i2+self.position] = 1 if current_state and (neighbors == 2 or neighbors == 3) else (1 if neighbors == 3 else 0)
+        self.current_grid = next_grid
+
+        self.past_ticks.append(self.live_cells()) # for tick reverse
+
+    def tick_reverse(self):
+        next_grid = np.zeros((self.size, self.size))
+        try:
+            past_tick_grid = self.past_ticks[-1] # 'coordinates' of active cells of past boards
+            for tup in past_tick_grid:
+                next_grid[tup[0]][tup[1]] = 1
+            del self.past_ticks[-1]
+            self.current_grid = next_grid
+        except IndexError:
+            pass
+        if len(self.past_ticks) > 100:
+            del self.past_ticks[0]
+
+    def expand(self):
+        """Stretching list to keep up with the amount of space the living cells take up. This function also
+            determines what part of the array to evaluate. After the section of the list that needs to be evaluated
+            is determined, the tick function evaluates the section and "puts it back" into the original array."""
+        live = self.live_cells()
+        live_x = [x[1] for x in live]
+        live_y = [y[0] for y in live]
+        try:
+            min_x = min(live_x)
+            min_y = min(live_y)
+            max_x = max(live_x)
+            max_y = max(live_y)
+            self.position = 1 if min_x == 0 or min_y == 0 else 0
+            self.position2 += 1 if min_x == 0 or min_y == 0 else 0
+            live_max_overall = max(max_x, max_y) + 3
+            self.size = live_max_overall
+            self.section_tobe_evaluated = (min_x, max_x + 1, min_y, max_y + 1)
+        except ValueError:
+            pass
+
+    def live_cells(self):
+        living = []
+        for y, row in enumerate(self.current_grid):
+            for x, cell in enumerate(row):
+                if cell:
+                    living.append((y, x))
+        return living
---
+++ main.py
@@ -0,0 +1,139 @@
+from tkinter.constants import LEFT
+from grid import Grid
+import tkinter as tk
+
+
+class Screen(tk.Canvas):
+    def __init__(self):
+        self.window_side_length = 800
+        self.window_shape_length = 200
+        tk.Canvas.__init__(self, root, height=self.window_side_length, width=self.window_side_length)
+        self.board = Grid()
+        self.board.new_pattern('glider')
+        self.ticks = 0
+        self.factors_of_300 = [x for x in range(1, 501) if self.window_side_length%x == 0 and x < 501] # to get equal spacing of the squares on the grid
+        self.action_state = 0 
+        self.separation_length = 0
+        self.moving_x = 0
+        self.moving_y = 0
+        self.slider = None
+        self.play_pause = None
+        self.patternlist = tk.StringVar()
+        self.patternlist.set('clear')
+
+        if self.action_state:
+            self.tick()
+
+    def todo(self, *action): 
+        if self.action_state:
+            self.action_state = 0
+            self.play_pause.config(text='Play')
+        else:
+            self.action_state = 1 
+            self.play_pause.config(text='Pause')
+
+    def place_squares(self):
+        self.delete('all')
+        self.create_rectangle(0, 0, self.window_side_length, self.window_side_length, fill='black')
+
+        if self.window_side_length % self.slider.get() != 0:  
+            closest = sorted(self.factors_of_300, key=lambda x: abs(x - self.slider.get()))
+            self.separation_length = self.window_shape_length / closest[0]
+
+        else:
+            self.separation_length = self.window_shape_length / self.slider.get()
+        m = (self.board.position2 * self.separation_length) - 15 
+        x = self.moving_x
+        y = self.moving_y-m
+        x2 = self.separation_length+self.moving_x
+        y2 = self.separation_length+self.moving_y-m
+        cube_array = self.board.current_grid 
+        for row in cube_array:
+            for block in row:
+                if block:
+                    self.create_rectangle(x-m, y, x2-m, y2, fill='white')
+
+                if not block:
+                    pass
+                x += self.separation_length
+                x2 += self.separation_length
+            x = self.moving_x
+            x2 = self.separation_length+self.moving_x
+            y += self.separation_length
+            y2 += self.separation_length
+
+        if self.action_state:
+            self.tick()
+        self.after(10, self.place_squares)
+
+    def tick(self):
+        self.ticks += 1
+        self.board.tick()
+
+    def reverse_tick(self):
+        self.board.tick_reverse()
+
+    def start_over(self):
+        self.ticks = 0
+        self.board.start_over()
+
+    def home(self):
+        self.moving_x = 0
+        self.moving_y = 0
+
+        self.slider.set(25)
+
+    def move(self, action):
+        if action.keycode == 37:
+            self.moving_x += 20
+        elif action.keycode == 38:
+            self.moving_y += 20
+        elif action.keycode == 39:
+            self.moving_x -= 20
+        elif action.keycode == 40:
+            self.moving_y -= 20
+
+    def set_pattern(self):
+        self.board.new_pattern(self.patternlist.get())
+
+
+def main():
+    canv = Screen()
+    canv.grid(column=2)
+    #tk.Frame(root, width=100).grid(column=0)
+    tk.Button(root, text='<', command=canv.reverse_tick).grid(column=2, row=1, sticky=tk.W)
+    f = tk.Frame(root)
+    f.grid(column=2, row=1)
+    play_pause = tk.Button(f, text='Play', command=canv.todo)
+    play_pause.pack(side=LEFT, expand=True)
+    tk.Button(f, text='Start Over', command=canv.start_over).pack(side=LEFT, expand=True)
+    tk.Button(f, text='Home', command=canv.home).pack(side=LEFT, expand=True)
+    tk.Button(root, text='>', command=canv.tick).grid(column=2, row=1, sticky=tk.E)
+    frame = tk.LabelFrame(root, text='Operations')
+    frame.grid(column=2)
+    tk.Label(frame, text='Zoom').grid(row=0, column=0)
+    slider = tk.Scale(frame, from_=1, to=150, orient=tk.HORIZONTAL)
+    slider.grid(row=0, column=1)
+    slider.set(25)
+    tk.Label(frame, text='Select Pattern').grid(row=1, column=0)
+    options = tk.OptionMenu(frame, canv.patternlist, 'clear', 'glider', 'glider 2', 'glider gun', 'random')
+    options.grid(row=1, column=1)
+    tk.Button(frame, text='Go', command=canv.set_pattern).grid(row=1, column=2)
+    canv.play_pause = play_pause
+    canv.slider = slider
+    canv.place_squares()
+    frame2 = tk.LabelFrame(frame)
+    frame2.grid(row=2, columnspan=3, pady=20)
+  
+    root.bind('<Down>', canv.move)
+    root.bind('<Left>', canv.move)
+    root.bind('<Right>', canv.move)
+    root.bind('<Up>', canv.move)
+    root.bind('<space>', canv.todo)
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title('CODIATION')
+    #root.geometry('500x500')
+    main()
+    root.mainloop()
